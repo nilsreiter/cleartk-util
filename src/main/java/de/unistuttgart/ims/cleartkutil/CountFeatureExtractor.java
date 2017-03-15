cdc1ed7cb9e1cd090d9df8b5935fa5491856d27b
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-03-15 11:06:34 +0100

    new feature extractors

---
+++ src/main/java/de/unistuttgart/ims/cleartkutil/CountFeatureExtractor.java
@@ -0,0 +1,29 @@
+package de.unistuttgart.ims.cleartkutil;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.cleartk.ml.Feature;
+import org.cleartk.ml.feature.extractor.CleartkExtractorException;
+import org.cleartk.ml.feature.extractor.FeatureExtractor1;
+
+public class CountFeatureExtractor<T extends Annotation> implements FeatureExtractor1<T> {
+
+	Class<? extends Annotation> goalClass;
+
+	public CountFeatureExtractor(Class<? extends Annotation> goalClass) {
+		super();
+		this.goalClass = goalClass;
+	}
+
+	public List<Feature> extract(JCas view, T focusAnnotation) throws CleartkExtractorException {
+		Feature f = new Feature();
+		f.setValue(JCasUtil.selectCovered(goalClass, focusAnnotation));
+		f.setName("count-" + goalClass.getSimpleName());
+		return Arrays.asList(f);
+	}
+
+}
